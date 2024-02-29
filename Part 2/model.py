38944f077ec5f07933e703365e38b20605781597
aakashv100 <vardhan.aakash1@gmail.com>
2024-02-29 19:35:57 +0400

    Fix convolutional block in model.py

--- Part 2/model.py
+++ Part 2/model.py
@@ -54,8 +54,8 @@ class Net(nn.Module):
         
         # Conv Block 2
         self.conv5 = ConvBlock(n_channels // 2, n_channels // 2, dropout=0, kernel_size=3)
-        self.conv6 = ConvBlock(n_channels // 2, n_channels, dropout=0.1, kernel_size=3)
-        self.conv7 = ConvBlock(n_channels, n_channels, dropout=0.1, kernel_size=3)
+        self.conv6 = ConvBlock(n_channels // 2, n_channels // 2, dropout=0.1, kernel_size=3)
+        self.conv7 = ConvBlock(n_channels // 2, n_channels, dropout=0.1, kernel_size=3)
         
         # Output Block
 
